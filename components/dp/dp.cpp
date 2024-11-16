186ea99c88c91151938a1e4a34287832b2932b61
mercix <marcin.slupski@gmail.com>
2024-11-16 14:15:36 +0100

    Update dp.cpp

--- components/dp/dp.cpp
+++ components/dp/dp.cpp
@@ -1,92 +1,38 @@
 #include "dp.h"
 
-DP::DP(UARTComponent *parent) : PollingComponent(5000), UARTDevice(parent) {
-  compressor = new binary_sensor::BinarySensor();
-  fan_low = new binary_sensor::BinarySensor();
-  fan_high = new binary_sensor::BinarySensor();
-  circulation_pump_hp = new binary_sensor::BinarySensor();
-  supplementary_heating = new binary_sensor::BinarySensor();
-  alarm_led = new binary_sensor::BinarySensor();
-}
-
-void DP::setup() {
-  // Additional setup if needed
-}
-
 void DP::update() {
   if (available()) {
-    readData();
+    read_data();
   }
-  publishSensorStates();
 }
 
-void DP::readData() {
+void DP::read_data() {
   const size_t maxBytes = 10;
   char hexString[(2 * maxBytes) + 1];
   char *hexPtr = hexString;
-  bool validStartByte = false;
-  bool valid6thByte = false;
 
   while ((hexPtr - hexString) < (2 * maxBytes) && available()) {
     uint8_t byte;
     if (read_byte(&byte)) {
-      if ((hexPtr - hexString) == 0 && byte == 0xFA) {
-        validStartByte = true;
-      }
-
-      if (validStartByte) {
-        hexPtr += snprintf(hexPtr, 3, "%02X", byte);
-        ESP_LOGD("CustomSensor", "Received byte: %02X", static_cast<unsigned>(byte));
-
-        if ((hexPtr - hexString) == 12 && byte == 0x00) {
-          valid6thByte = true;
-        }
-      }
+      hexPtr += snprintf(hexPtr, 3, "%02X", byte);
     }
   }
 
   *hexPtr = '\0';
-
-  if (validStartByte && valid6thByte && (hexPtr - hexString) > 0) {
-    ESP_LOGD("CustomSensor", "Received data: %s", hexString);
-    processHexString(hexString);
-  } else {
-    ESP_LOGD("CustomSensor", "Invalid data received.");
-  }
+  process_data(hexString);
 }
 
-void DP::processHexString(char *hexString) {
-  std::vector<uint8_t> h_byte;
-  std::vector<std::vector<bool>> b_byte;
+void DP::process_data(const char *hex_data) {
+  if (strlen(hex_data) < 12) return;
 
-  for (size_t x = 2; x < 6; x += 2) {
-    h_byte.push_back(static_cast<uint8_t>(std::stoi(std::string(hexString + x, 2), nullptr, 16)));
-    std::vector<bool> bits = getBits(h_byte.back());
+  uint8_t byte1 = strtol(std::string(hex_data + 2, 2).c_str(), nullptr, 16);
+  uint8_t byte2 = strtol(std::string(hex_data + 4, 2).c_str(), nullptr, 16);
 
-    ESP_LOGD("CustomSensor", "Binary representation for Byte %zu:", x / 2 - 1);
-    for (size_t i = 0; i < bits.size(); ++i) {
-      ESP_LOGD("CustomSensor", "   Bit %zu: %d", i, bits[i] ? 1 : 0);
-    }
-
-    b_byte.push_back(bits);
-  }
-
-  compressor->publish_state(b_byte[0][0]);
-  fan_low->publish_state(b_byte[0][1]);
-  fan_high->publish_state(b_byte[0][3]);
-  circulation_pump_hp->publish_state(b_byte[1][1]);
-  supplementary_heating->publish_state(b_byte[1][5]);
-  alarm_led->publish_state(b_byte[1][0]);
+  if (compressor_) compressor_->publish_state(byte1 & 0x01);
+  if (fan_low_) fan_low_->publish_state(byte1 & 0x02);
+  if (fan_high_) fan_high_->publish_state(byte1 & 0x08);
+  if (alarm_led_) alarm_led_->publish_state(byte2 & 0x01);
+  if (circulation_pump_hp_) circulation_pump_hp_->publish_state(byte2 & 0x02);
+  if (supplementary_heating_) supplementary_heating_->publish_state(byte2 & 0x20);
 }
 
-std::vector<bool> DP::getBits(uint8_t value) {
-  std::vector<bool> bits(8);
-  for (int i = 7; i >= 0; --i) {
-    bits[i] = (value & (1 << (7 - i))) != 0;
-  }
-  return bits;
-}
-
-void DP::publishSensorStates() {
-  // Implement sensor state publishing logic if needed
-}
