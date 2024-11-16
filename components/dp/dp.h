d9ac609a6fccb6a8b13298744f3d5c99420eca7d
mercix <marcin.slupski@gmail.com>
2024-11-16 14:14:59 +0100

    Update dp.h

--- components/dp/dp.h
+++ components/dp/dp.h
@@ -1,24 +1,27 @@
 #include "esphome.h"
-#include <vector>
-#include <string>
 
 class DP : public PollingComponent, public UARTDevice {
 public:
-  DP(UARTComponent *parent);
+  DP() : PollingComponent(5000) {}
 
-  binary_sensor::BinarySensor *compressor;
-  binary_sensor::BinarySensor *fan_low;
-  binary_sensor::BinarySensor *fan_high;
-  binary_sensor::BinarySensor *circulation_pump_hp;
-  binary_sensor::BinarySensor *supplementary_heating;
-  binary_sensor::BinarySensor *alarm_led;
+  void set_compressor_sensor(binary_sensor::BinarySensor *sensor) { compressor_ = sensor; }
+  void set_fan_low_sensor(binary_sensor::BinarySensor *sensor) { fan_low_ = sensor; }
+  void set_fan_high_sensor(binary_sensor::BinarySensor *sensor) { fan_high_ = sensor; }
+  void set_circulation_pump_hp_sensor(binary_sensor::BinarySensor *sensor) { circulation_pump_hp_ = sensor; }
+  void set_supplementary_heating_sensor(binary_sensor::BinarySensor *sensor) { supplementary_heating_ = sensor; }
+  void set_alarm_led_sensor(binary_sensor::BinarySensor *sensor) { alarm_led_ = sensor; }
 
-  void setup() override;
+  void setup() override {}
   void update() override;
 
 private:
-  void readData();
-  void processHexString(char *hexString);
-  void publishSensorStates();
-  std::vector<bool> getBits(uint8_t value);
+  binary_sensor::BinarySensor *compressor_;
+  binary_sensor::BinarySensor *fan_low_;
+  binary_sensor::BinarySensor *fan_high_;
+  binary_sensor::BinarySensor *circulation_pump_hp_;
+  binary_sensor::BinarySensor *supplementary_heating_;
+  binary_sensor::BinarySensor *alarm_led_;
+
+  void read_data();
+  void process_data(const char *hex_data);
 };
