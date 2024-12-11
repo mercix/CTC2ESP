aab4c7664350cf8c49e3fdb4031bc8d78f7fcef8
mercix <marcin.slupski@gmail.com>
2024-12-11 17:20:31 +0100

    Update dp_custom.h

--- components/dp_custom/dp_custom.h
+++ components/dp_custom/dp_custom.h
@@ -3,18 +3,22 @@
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
-#include "esphome/core/log.h"
+#include <vector>
 
-class DP : public esphome::PollingComponent, public esphome::uart::UARTDevice {
+namespace esphome {
+namespace dp_custom {
+
+class DP : public PollingComponent, public uart::UARTDevice {
 public:
-  DP(esphome::uart::UARTComponent *parent);
+  DP(uart::UARTComponent *parent);
 
-  esphome::binary_sensor::BinarySensor *compressor;
-  esphome::binary_sensor::BinarySensor *fan_low;
-  esphome::binary_sensor::BinarySensor *fan_high;
-  esphome::binary_sensor::BinarySensor *circulation_pump_hp;
-  esphome::binary_sensor::BinarySensor *supplementary_heating;
-  esphome::binary_sensor::BinarySensor *alarm_led;
+  // Binary sensor declarations
+  binary_sensor::BinarySensor *compressor;
+  binary_sensor::BinarySensor *fan_low;
+  binary_sensor::BinarySensor *fan_high;
+  binary_sensor::BinarySensor *circulation_pump_hp;
+  binary_sensor::BinarySensor *supplementary_heating;
+  binary_sensor::BinarySensor *alarm_led;
 
   void setup() override;
   void update() override;
@@ -23,3 +27,6 @@ public:
   std::vector<bool> getBits(uint8_t value);
   void publishSensorStates();
 };
+
+}  // namespace dp_custom
+}  // namespace esphome
