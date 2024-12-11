f4c087906463d1f7729717bea2c94047e6893df1
mercix <marcin.slupski@gmail.com>
2024-12-11 17:03:17 +0100

    Create dp_custom.h

---
+++ components/dp_custom/dp_custom.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "esphome.h"
+#include <sstream>
+
+class DP : public PollingComponent, public UARTDevice {
+public:
+  DP(UARTComponent *parent);
+
+  binary_sensor::BinarySensor *compressor;
+  binary_sensor::BinarySensor *fan_low;
+  binary_sensor::BinarySensor *fan_high;
+  binary_sensor::BinarySensor *circulation_pump_hp;
+  binary_sensor::BinarySensor *supplementary_heating;
+  binary_sensor::BinarySensor *alarm_led;
+
+  void setup() override;
+  void update() override;
+  void readData();
+  void processHexString(char* hexString);
+  std::vector<bool> getBits(uint8_t value);
+  void publishSensorStates();
+};
