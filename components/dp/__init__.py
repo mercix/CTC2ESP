21ef37f67da3ccff7ad2ba0bed759e493abfb89a
mercix <marcin.slupski@gmail.com>
2024-12-16 18:46:59 +0100

    Update __init__.py

--- components/dp/__init__.py
+++ components/dp/__init__.py
@@ -1,12 +1,12 @@
 import esphome.codegen as cg
 import esphome.components.binary_sensor as binary_sensor
-import esphome.components.uart as uart  # Import uart for UARTDevice
+import esphome.components.uart as uart
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_UART_ID
 
 DEPENDENCIES = ['uart']
 
-# Define the DP class
+# Define the namespace and class for the DP component
 dp_ns = cg.esphome_ns.namespace('dp')
 DP = dp_ns.class_('DP', cg.PollingComponent, uart.UARTDevice)
 
@@ -16,10 +16,12 @@ CONFIG_SCHEMA = cv.Schema({
 }).extend(cv.polling_component_schema('5s'))
 
 def to_code(config):
+    # Retrieve the UART component using the correct type
     uart_component = yield cg.get_variable(config[CONF_UART_ID])
     var = cg.new_Pvariable(config[CONF_ID], uart_component)
+    
     yield cg.register_component(var, config)
-    yield uart.register_uart_device(var, uart_component)
+    yield uart.register_uart_device(var, uart_component)  # Register UART device correctly
 
     # Define and attach binary sensors
     sensors = {
@@ -32,6 +34,6 @@ def to_code(config):
     }
 
     for attr_name, sensor_name in sensors.items():
-        sens = binary_sensor.BinarySensor.new()
+        sens = cg.new_Pvariable(binary_sensor.BinarySensor())
         cg.add(sens.set_name(sensor_name))
-        cg.add(getattr(var, attr_name), sens)
+        setattr(var, attr_name, sens)  # Attach the sensor to the DP instance
