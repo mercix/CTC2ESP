52dba487376faa9c609cbac1a2637cded113ec2f
mercix <marcin.slupski@gmail.com>
2024-12-16 19:00:45 +0100

    Update dp.h

--- components/dp/dp.h
+++ components/dp/dp.h
@@ -1,10 +1,12 @@
 #include "esphome.h"
-#include <sstream>
+#include "esphome/components/binary_sensor/binary_sensor.h"  // Include binary sensor
+#include "esphome/components/uart/uart.h"  // Include UART
 
 class DP : public PollingComponent, public UARTDevice {
 public:
   DP(UARTComponent *parent) : PollingComponent(5000), UARTDevice(parent) {}
-  
+
+  // Declare binary sensors
   binary_sensor::BinarySensor *compressor = new binary_sensor::BinarySensor();
   binary_sensor::BinarySensor *fan_low = new binary_sensor::BinarySensor();
   binary_sensor::BinarySensor *fan_high = new binary_sensor::BinarySensor();
@@ -13,10 +15,11 @@ public:
   binary_sensor::BinarySensor *alarm_led = new binary_sensor::BinarySensor();
 
   void setup() override {
-    // Additional setup if needed
+    // Initialize sensors or perform any setup logic here
   }
 
   void update() override {
+    // This function is periodically called (every 5000ms)
     if (available()) {
       readData();
     }
@@ -24,10 +27,12 @@ public:
   }
 
   void readData() {
-    // UART reading logic
+    // Read data from UART and process it
+    // Implement your UART reading logic here
   }
 
   void publishSensorStates() {
-    // Publish sensor states
+    // Publish the sensor states (e.g., compressor, fan_low, etc.)
+    // Implement your logic to publish sensor states here
   }
 };
