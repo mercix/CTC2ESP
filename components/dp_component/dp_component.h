47ad040e1babe23a391767c1ca7b648b8a97c549
mercix <marcin.slupski@gmail.com>
2025-01-03 22:04:29 +0100

    Update dp_component.h

--- components/dp_component/dp_component.h
+++ components/dp_component/dp_component.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "esphome.h"
-#include <vector>
 
 namespace esphome {
 namespace dp_component {
@@ -9,12 +8,12 @@ class DpComponent : public PollingComponent, public UARTDevice {
  public:
   DpComponent(UARTComponent *parent) : PollingComponent(5000), UARTDevice(parent) {}
 
-  void set_compressor(binary_sensor::BinarySensor *sensor) { compressor = sensor; }
-  void set_fan_low(binary_sensor::BinarySensor *sensor) { fan_low = sensor; }
-  void set_fan_high(binary_sensor::BinarySensor *sensor) { fan_high = sensor; }
-  void set_circulation_pump_hp(binary_sensor::BinarySensor *sensor) { circulation_pump_hp = sensor; }
-  void set_supplementary_heating(binary_sensor::BinarySensor *sensor) { supplementary_heating = sensor; }
-  void set_alarm_led(binary_sensor::BinarySensor *sensor) { alarm_led = sensor; }
+  void set_compressor(binary_sensor::BinarySensor *sensor) { this->compressor = sensor; }
+  void set_fan_low(binary_sensor::BinarySensor *sensor) { this->fan_low = sensor; }
+  void set_fan_high(binary_sensor::BinarySensor *sensor) { this->fan_high = sensor; }
+  void set_circulation_pump_hp(binary_sensor::BinarySensor *sensor) { this->circulation_pump_hp = sensor; }
+  void set_supplementary_heating(binary_sensor::BinarySensor *sensor) { this->supplementary_heating = sensor; }
+  void set_alarm_led(binary_sensor::BinarySensor *sensor) { this->alarm_led = sensor; }
 
   void setup() override;
   void update() override;
@@ -26,10 +25,6 @@ class DpComponent : public PollingComponent, public UARTDevice {
   binary_sensor::BinarySensor *circulation_pump_hp{nullptr};
   binary_sensor::BinarySensor *supplementary_heating{nullptr};
   binary_sensor::BinarySensor *alarm_led{nullptr};
-
-  void read_data();
-  void process_hex_string(const char *hex_string);
-  std::vector<bool> get_bits(uint8_t value);
 };
 
 }  // namespace dp_component
